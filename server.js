f7c76c20146a9b60113386cead952c3a57ba38af
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-07-15 13:28:16 -0400

    removed additional server.js stuff

--- server.js
+++ server.js
@@ -5,17 +5,6 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { flights } = require('./test-data/flightSeating');
 
-const PORT = process.env.PORT || 8000;
-
-const handleFlight = (req, res) => {
-    const { flightNumber } = req.params;
-    // get all flight numbers
-    const allFlights = Object.keys(flights);
-    // is flightNumber in the array?
-    console.log('REAL FLIGHT: ', allFlights.includes(flightNumber))
-}
-
-{name: 'Fred'}
 express()
     .use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
@@ -28,6 +17,5 @@ express()
     .use(express.urlencoded({extended: false}))
     
     // endpoints
-    .get('/flights/:flightNumber', handleFlight)
     .use((req, res) => res.send('Not Found'))
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    .listen(PORT, () => console.log(`Listening on port 8000`));
